13ec80b0ee7baa930cd53811a38fad488e3fad8b
Claudio Poblete <pobleteclaudio2103@gmail.com>
2024-01-29 23:43:03 -0300

    Se eliminan imagenes, se agregan nuevas/ se agregan nuevos estilos y se termina about section y project section

---
+++ assets/js/action.js
@@ -0,0 +1,10 @@
+document.addEventListener('scroll', () =>{
+  const nav = document.querySelector('nav')
+
+  if (window.scrollY > 100) {
+    nav.classList.add('scroll')
+  } else {
+    nav.classList.remove('scroll')
+  }
+})
+
